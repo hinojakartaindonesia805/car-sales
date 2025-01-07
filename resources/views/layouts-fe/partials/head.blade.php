528803ab05071fee4a698d4c075bf0803d220f93
adl.tomie <adl.tomie@xl.co.id>
2025-01-07 09:06:25 +0700

    fix

--- resources/views/layouts-fe/partials/head.blade.php
+++ resources/views/layouts-fe/partials/head.blade.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
-<meta name="keywords" content="Hino Jakarta Indonesia, Dealer Hino, Truk Hino, Bus Hino, Dealer Resmi Hino, Jual Truk">
+<meta name="keywords" content="Hino Jakarta Indonesia, Dealer Hino, Truk Hino, Bus Hino, Dealer Resmi Hino, Jual Truk Hino">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <title>HINO Jakarta Indonesia - Dealer Resmi Hino</title>
